dd4339ebdb6a633cc648d20307c47c8428569c52
waltidor-06Wigensky Altidor <82045347+waltidor-06@users.noreply.github.com>
2021-08-02 23:13:16 -0400

    Adds app from VS code

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: "wigensky-secret-key",
+};
---
+++ app/config/db.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "postgres",
+    DB: "testdb",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
